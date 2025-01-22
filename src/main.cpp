81e4e68a7c80fdd1d725622fc44553ffca4ca99f
Gijs-De-Munck <gijs.eend@gmail.com>
2025-01-22 20:44:45 +0100

    refactor motor control logic to use serial input for step values

--- src/main.cpp
+++ src/main.cpp
@@ -7,38 +7,36 @@ Stepper az_stepper(200, 6, 7, 8, 9);
 int alt_steps = 0;
 bool going_up = true;
 
-void moveAzStepper();
-
 void setup() {
+  Serial.begin(9600);
   alt_stepper.setSpeed(1);
   az_stepper.setSpeed(1);
 }
 
-void moveAltStepper() {
-  if (going_up) {
-    alt_stepper.step(12);
-    alt_steps += 3;
-    delay(10000); // 10 seconds
+void loop() {
+  if (Serial.available() > 0) {
+    String input = Serial.readStringUntil('\n');
+    int commaIndex = input.indexOf(',');
+    int alt_step_value = input.substring(0, commaIndex).toInt();
+    int az_step_value = input.substring(commaIndex + 1).toInt();
 
-    if (alt_steps >= 201) {
-      moveAzStepper();
-      going_up = false;
-    }
-  } else {
-    alt_stepper.step(-12);
-    alt_steps -= 3;
-    delay(10000); // 10 seconds
+    if (going_up) {
+      alt_stepper.step(alt_step_value);
+      alt_steps += alt_step_value;
+      delay(10000); // 10 seconds
 
-    if (alt_steps <= 0) {
-      going_up = true;
+      if (alt_steps >= 201) {
+        az_stepper.step(az_step_value); // Move the second motor
+        going_up = false;
+      }
+    } else {
+      alt_stepper.step(-alt_step_value);
+      alt_steps -= alt_step_value;
+      delay(10000); // 10 seconds
+
+      if (alt_steps <= 0) {
+        going_up = true;
+      }
     }
   }
-}
-
-void moveAzStepper() {
-  az_stepper.step(6); // Move the second motor 6 degrees
-}
-
-void loop() {
-  moveAltStepper();
 }
